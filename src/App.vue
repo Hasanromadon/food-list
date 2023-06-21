bb7a931e68943b4fdcdf3b28bd96566deea44ee4
Hasan Romadon <hsanromadon@gmail.com>
2023-06-21 23:57:53 +0700

    chore: setting up vuex

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
-<script setup>
-</script>
-
 <template>
-<h1 className="text-3xl font-bold underline">
-      Hello world!
-</h1>
+      <router-view/>
 </template>
 
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -12,4 +12,6 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
-})
+});
+
+export default router;
