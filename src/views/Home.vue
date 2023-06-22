1b9314d1ce9e7aaade76ee38020102b4dc74d970
Hasan Romadon <hsanromadon@gmail.com>
2023-06-22 23:32:10 +0700

    feat: create homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,15 @@
 <template>
-  <pre>{{ meals }}</pre>
+  <div class=" w-full p-4 flex justify-center">
+    <input type="text" class=" w-3/4 border-2 border-gray-300 rounded-sm" placeholder="Search foods..." />
+  </div>
+  <div class="flex gap-1 justify-center">
+    <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
+      {{ letter }}
+    </router-link>
+  </div>
 </template>
 
 <script setup>
-import {computed} from 'vue'
-import store from '../store'
-const meals = computed(()=> store.state.meals)
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 
 </script>
---
+++ src/views/MealsList.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        meals list
+    </div>
+</template>
+
+<script setup>
+</script>
